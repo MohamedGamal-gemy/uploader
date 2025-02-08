154d8e6de9c8ce182f68382e39f1962978ad6a9b
MohamedGamal-gemy <mg7171362@gmail.com>
2025-02-08 03:02:48 +0200

    Your commit message7

--- src/utils/AdminEmailContext .jsx
+++ src/utils/AdminEmailContext .jsx
@@ -3,10 +3,9 @@ import React, { createContext, useState } from "react";
 const AdminEmailContext = createContext();
 
 // samaalatwi23@gmail.com
-// ahmedaliahmedgenedy@gmail.com
 // ga863410@gmail.com
 export const AdminEmailProvider = ({ children }) => {
-  const [adminEmail, setAdminEmail] = useState("samaalatwi23@gmail.com");
+  const [adminEmail, setAdminEmail] = useState("ga863410@gmail.com");
 
   return (
     <AdminEmailContext.Provider value={{ adminEmail, setAdminEmail }}>
